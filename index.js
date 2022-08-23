1cc7a6855e0704060b1eda40e05b5a4bc7f81a34
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-23 21:25:05 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+var multiply=(num1,num2)
+let num1=31;
+let num2=2;
+let z = (num1*num2)
+//expects 62
+
+var integer=i>0
+function getRandomInt(min,max) {
+    
+}
+//generetas i>0
+
+let num3=16;
+let num4=4;
+ let n =(num3%num4) //expects 4
+
+
+ let x = Math.max(5,10,15,20) //returns 20
